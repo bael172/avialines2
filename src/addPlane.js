d8cecab26bd5abe0bdc8c5cf70777d3213c51cc4
Абдималиков Байэл Эрнистович <b.abdimalikov@mkit.local>
2025-02-13 16:53:11 +0300

    fix: Изменен addPlane.js Ошибка RequestStatusCode 404

--- src/addPlane.js
+++ src/addPlane.js
@@ -1,25 +1,31 @@
 import './addPlane.css'
-import {useContext, useState, useEffect} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {observer} from 'mobx-react-lite'
 import { userContext } from './index.js'
 import {add_plane} from './http/plane_queries.js'
-const context = useContext(userContext)
+
 const App = observer(()=>{
+    const context = useContext(userContext)
+
+    const [plane_id,setPlaneId] = useState('')
     const [plane_type,setType] = useState('')
-    const [photo,setPhoto] = useState('')
-    const [serial,setSerial] = useState('')
-    const [plane_name,setName] = useState('')
-    const [seats_number,setSeatsNumber] = useState('')
-    const [classes,setClasses] = useState('')
-    const [avialines,setAvialines] = useState('')
-    const [entries_number,setEntriesNumber] = useState('')
-    const [luggage_capacity,setLuggageCapacity] = useState('')
-    const []
+    const [photo, setPhoto] = useState('')
+    const [serial, setSerial] = useState('')
+    const [plane_name, setName] = useState('')
+    const [seats_number, setSeatsNumber] = useState(null)
+    const [classes, setClasses] = useState('')
+    const [avialines, setAvialines] = useState('')
+    const [entries_number, setEntriesNumber] = useState(null)
+    const [luggage_capacity, setLuggageCapacity] = useState(null)
+    const [fueltank_capacity, setFueltankCapacity] = useState(null)
+    const [current_fuel_lvl, setCurrentFuelLevel] = useState(null)
+    const [crew_member_count, setCrewMemberCount] = useState(null)
+    const [status, setStatus] = useState('')
     const add = async(e)=>{
         e.preventDefault()
         try{
-            add_plane(id,serial,pfp,plane_type,plane_name,classes,airline,
-                        seats_number,entries_number,crew_member_number,luggage_capacity,
+            add_plane(plane_id,serial,plane_type,photo,plane_name,classes,avialines,
+                        seats_number,entries_number,crew_member_count,luggage_capacity,
                         fueltank_capacity,current_fuel_lvl,status).then(
                         response => {
                             context.store.setUserRequest(response)
@@ -28,65 +34,76 @@ const App = observer(()=>{
         }
         catch(error){
             console.error('status:',error.status,' message',error.message)
+            console.log(error)
         }
     } 
     return(
-        <form onSubmit={add} method="post">
+        <div className="App">
+            <form onSubmit={add} method="post" id="form_post_plane">
+            <label>Бортовой номер (номер гос.регистрации/ID самолёта)
+                <input type="text" id='plane_id' value={plane_id}
+                onChange={(e)=>{setPlaneId(e.target.value)}}></input>
+            </label>
             <label>Тип самолёта
-                <input type="text" id={aeroplane_type} value={plane_type}
+                <input type="text" id='aeroplane_type' value={plane_type}
                 onChange={(e)=>{setType(e.target.value)}}></input>
             </label>
             <label>Серийный номер
-                <input type="text" id={serial} value={serial}
-                onChange={(e)=>{setType(e.target.value)}}></input>
+                <input type="text" id='serial' value={serial}
+                onChange={(e)=>{setSerial(e.target.value)}}></input>
             </label>
             <label>Фото самолёта
-                <input type="file" id={plane_photo} value={photo}></input>
-                onChange={(e)=>{setPhoto(e.target.value)}}
+                <input type="file" accept="image/*" id='plane_photo'
+                onChange={(e)=>{setPhoto(e.target.files[0])}}></input>
             </label>
+            <div>{JSON.stringify(photo)}</div>
             <label>Название самолёта
-                <input type="text" id={plane_name} value={plane_name}></input>
-                onChange={(e)=>{setName(e.target.value)}}
+                <input type="text" id='plane_name' value={plane_name}
+                onChange={(e)=>{setName(e.target.value)}}></input>
             </label>
             <label>Кол-во сидений
-                <input type="number" id={seats_number} value={seats_number}
+                <input type="number" id='seats_number' value={seats_number}
                 onChange={(e)=>{setSeatsNumber(e.target.value)}}></input>
             </label>
-            <label>Классы
-                <input type="checkbox" id={econom} value={classes} onChange={(e)=>{setClasses(e.target.value)}}></input>
-                <input type="checkbox" id={business} value={classes} onChange={(e)=>{setClasses(e.target.value)}}></input>
-                <input type="checkbox" id={vip} value={classes} onChange={(e)=>setClasses(e.target.value)}></input>
+            <label class="classes">Классы
+                <label><input type="checkbox" id='econom' placeholder='Эконом' value={classes} onChange={(e)=>{setClasses(+e.target.value)}}></input>Эконом</label>
+                <label><input type="checkbox" id='business' placeholder='Бизнес' value={classes} onChange={(e)=>{setClasses(+e.target.value)}}></input>Бизнес</label>
+                <label><input type="checkbox" id='vip' placeholder='VIP' value={classes} onChange={(e)=>setClasses(+e.target.value)}></input>VIP</label>
             </label>
             <label>Авиалинии
-                <input type="text" id={aeroplane_name} value={avialines}></input>
-                onChange={(e)=>{setAvialines(e.target.value)}}
+                <input type="text" id='aeroplane_name' value={avialines}
+                onChange={(e)=>{setAvialines(e.target.value)}}></input>
             </label>
             <label>Кол-во входов
-                <input type="number" id={plane_name} value={entries_number}></input>
-                onChange={(e)=>{setEntriesNumber(e.target.value)}}
+                <input type="number" id='plane_name' value={entries_number}
+                onChange={(e)=>{setEntriesNumber(e.target.value)}}></input>
             </label>
-            <label>Ёмкость багажного отсека (литров)
-                <input type="text" id={luggage_capacity} value={luggage_capacity}></input>
-                onChange={(e)=>{setLuggageCapacity(e.target.value)}}
+            <label>Ёмкость багажного отсека (в литрах)
+                <input type="text" id='luggage_capacity' value={luggage_capacity}
+                onChange={(e)=>{setLuggageCapacity(e.target.value)}}></input>
             </label>
-            <label>Ёмкость бензобака (литров)
-                <input type="text" id={plane_name} value={fueltank_capacity}></input>
-                onChange={(e)=>{setFueltankCapacity(e.target.value)}}
+            <label>Ёмкость бензобака (в литрах)
+                <input type="text" id='plane_name' value={fueltank_capacity}
+                onChange={(e)=>{setFueltankCapacity(e.target.value)}}></input>
             </label>
-            <label>Текущий уровень топлива (литров)
-                <input type="number" id={current_fuel_level} value={current_fuel_lvl}></input>
-                onChange={(e)=>{setCurrentFuelLevel(e.target.value)}}
+            <label>Текущий уровень топлива (в литрах)
+                <input type="number" id='current_fuel_level' value={current_fuel_lvl}
+                onChange={(e)=>{setCurrentFuelLevel(e.target.value)}}></input>
             </label>
             <label>Кол-во членов экипажа
-                <input type="number" id={crew_member_count} value={crew_member_count}
+                <input type="number" id='crew_member_count' value={crew_member_count}
                 onChange={(e)=>setCrewMemberCount(e.target.value)}></input>
             </label>
             <label>Статус
-                <input type="text" id={plane_status} value={status}></input>
-                onChange={(e)=>{setStatus(e.target.value)}}
+                <input type="text" id='plane_status' value={status}
+                onChange={(e)=>{setStatus(e.target.value)}}></input>
             </label>
-            <button type="submit"></button>
-        </form>
+            <div class="half">
+                <button type="submit" class="submit" formAction={add}>Отправить</button>
+                <button type="reset" class="reset">Стереть</button>
+            </div>
+            </form>
+        </div>
     )
 })
 export default App
--- src/http/plane_queries.js
+++ src/http/plane_queries.js
@@ -1,6 +1,6 @@
 import {$host, $authHost} from './axios_connect.js'
 
-export const add_plane = async(id,serial,pfp,type,name,classes,airline,
+export const add_plane = async(id,serial,type,pfp,name,classes,airline,
         seats_number,entries_number,crew_member_number,luggage_capacity,
         fueltank_capacity,current_fuel_level,status)=>{
         const {data} = await $host.post('/query/plane/get_all',
