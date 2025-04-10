3e5246af129ae75aee455682f6ebffabbc85a29e
bael172 <128132808+bael172@users.noreply.github.com>
2025-04-10 16:32:26 +0300

    feat: Добавление комментариев для кеширования запроса Plane.getAll()

--- queries/plane.js
+++ queries/plane.js
@@ -151,44 +151,59 @@ class Samolet{
             return next(ApiError.internal(error))
         }
     }
-    async get_all(req,res,next){
+        async get_all(req,res,next){
         console.log('privet')
         try{
             const all = await Plane.findAll()
             const data = JSON.stringify(all)
             const etag = ETag(data)
             //const lastModified = new Date(all.updatedAt.toUTCSyting()); //Получаем дату из базы данных
+            //в итоге lastModified - массив объектов Date
             let lastModifiedDate = null;
-            for (const plane of all) {
+            for (let plane of all) {
             if (plane.updatedAt) {
                 const currentUpdatedAt = new Date(plane.updatedAt);
+                if(isNaN(currentUpdatedAt.getTime())){
+                    return next(ApiError.internal('Ошибка преобразование поля updatedAt в объект Date'))
+                }
+                //currentUpdatedAt - объект Date со значением последнего обновления записи Plane
+                //Перебор всех записей из таблицы Plane и запись в lastModifiedDate самой последней даты обновления Plane из всех записей
+                //Если текущее значение атрибута updateAt новее чем lastModifiedDate
+                //поздняя дата БОЛЬШЕ чем ранняя дата
                 if (lastModifiedDate===null || currentUpdatedAt > lastModifiedDate) {
-                lastModifiedDate = currentUpdatedAt;
+                    lastModifiedDate = currentUpdatedAt;
                 }
             }
             //!lastModified проверка на falsy значение (undefined,NaN,"",null,false)
             //!lastModified тоже самое что и lastModified == null
-            //поздняя дата БОЛЬШЕ чем ранняя дата
             } //Находим самую позднюю дату updatedAt
             if(lastModifiedDate){
-                const lastModified = lastModifiedDate.toUTCString();
-                res.setHeader('Last-Modified',lastModified)
+                const lastModified = lastModifiedDate.toUTCString(); //Преобразование даты в строку
+                res.setHeader('Last-Modified',lastModified);
             }
 
             //1 вариант кэширования
-            res.setHeader('Cache-Control','public,max-age=3600')
+            //res.setHeader('Cache-Control','public,max-age=3600')
             
             //2 вариант кэширования
             res.setHeader('ETag',etag);
 
-            //res.setHeader('Cache-Control','no-cache')
-
+            res.setHeader('Cache-Control','no-cache') //кэшируем после проверки того что ресурс изменился
+            
+            //ifNoneMatch возвращает null/undefined , * или одна/несколько строк ETag
             const ifNoneMatch = req.headers['If-None-Match'];
-            const ifModifiedSince = req.headers['If-Modified-Since']
+            console.log('If-None-Match = ',ifNoneMatch);
 
+            //Дата когда клиент в последний раз получал запрошенный ресурс
+            const ifModifiedSince = req.headers['If-Modified-Since'];
+
+            //Если значение ifNoneMatch совпадает с хэшом текущего ресурса
             if(ifNoneMatch === etag){
-                res.status(304).end; //Not Modified
+                res.status(304).end(); //Not Modified
             }
+
+            //Если дата последнего запроса ресурса позднее (новее) чем самая поздняя (новая) запись таблицы Plane
+            //то возвращаем значение из кэша
             if(ifModifiedSince && new Date(ifModifiedSince) >= lastModifiedDate){
                 return res.status(304).end; //Not Modified
             }
